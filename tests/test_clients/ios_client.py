f6a0f81d1890db983d821becb459ff6edd4c71e9
Matthew Wild <mwild1@gmail.com>
2021-02-04 11:14:49 +0000

    Add get_ios_version and get_device_type to iOS clients

--- tests/test_clients/ios_client.py
+++ tests/test_clients/ios_client.py
@@ -1,4 +1,9 @@
 from .generic_client import GenericClient
+import math
 
 class IOSClient(GenericClient):
-	pass
+	def get_ios_version(self):
+		return math.floor(float(self.caps["platformVersion"]))
+
+	def get_device_type(self):
+		return "iPad" if self.caps["deviceName"].startswith("iPad ") else "iPhone"
